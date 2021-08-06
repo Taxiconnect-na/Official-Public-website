91d2c9913280729a13fc2b00223092a2464fe336
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-08-06 17:26:24 +0200

    Leaflet integration

--- src/Component/Screens/ContactDesktop.js
+++ src/Component/Screens/ContactDesktop.js
@@ -1,6 +1,19 @@
 import classes from "../../styles/Contact_desktop.module.css";
 import Header from "../Header";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "../../styles/leaflet/leaflet.css";
+import L from "leaflet";
+
+const iconPerson = new L.Icon({
+  iconUrl: require("../../styles/leaflet/images/marker-icon.png"),
+  iconAnchor: null,
+  popupAnchor: null,
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(60, 75),
+  className: "leaflet-div-icon",
+});
 
 const ContactDesktop = () => {
   return (
@@ -8,18 +21,23 @@ const ContactDesktop = () => {
       <Header />
       <div className={classes.mapContainer}>
         <MapContainer
-          center={[51.505, -0.09]}
+          center={[-22.575694, 17.083251]}
           zoom={13}
           scrollWheelZoom={false}
+          style={{ height: "100%", width: "100%" }}
         >
           <TileLayer
-            attribution="1"
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='TaxiConnect | <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+            url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
+            accessToken="pk.eyJ1IjoiZG9taW5pcXVla3R0IiwiYSI6ImNrYXg0M3gyNDAybDgyem81cjZuMXp4dzcifQ.PpW6VnORUHYSYqNCD9n6Yg"
+            maxZoom={15}
+            minZoom={15}
+            id="mapbox/streets-v11"
+            tileSize={512}
+            zoomOffset={-1}
           />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
+          <Marker icon={iconPerson} position={[-22.575694, 17.083251]}>
+            <Popup>TaxiConnect Headquarters</Popup>
           </Marker>
         </MapContainer>
       </div>
