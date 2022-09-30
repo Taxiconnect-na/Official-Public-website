5190011392889b7e517c991b6cf5d8e87a5b1571
Dominique <domykanyiktesh01@gmail.com>
2022-09-30 11:45:29 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "npm start",
       autorestart: true,
       watch: false,
-      max_memory_restart: "2G",
+      max_memory_restart: "6G",
       env: {
         NODE_ENV: "production",
       },
